81b67aee4699673e3290ed0763f31bc3731b7609
ChrisCline749 <46876667+ChrisCline749@users.noreply.github.com>
2022-09-29 15:51:39 -0400

    clicking near slingshot creates projectile

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspecter")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aiming;
 
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
 
     }
     private void OnMouseEnter()
@@ -21,5 +29,27 @@ public class Slingshot : MonoBehaviour
     private void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //check if player has projectiles?
+
+        aiming = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (aiming == true)
+        {
+            //launch projectile
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+
+            aiming = false;
+            //count down 1 projectile use
+        }
     }
 }
